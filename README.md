23320001bb4bd6190fc7da5dfb3d5e130380a24a
A9FM <68541852+A9FM@users.noreply.github.com>
2022-03-31 23:41:07 +0300

    USE backup from 31-03-2022

---
+++ README.md
@@ -0,0 +1,2 @@
+# Сурсы сайта [a9fm.github.io](https://a9fm.github.io)
+## by [@a9fm](https://github.com/A9FM) 
---
+++ assets/js/text.js
@@ -0,0 +1,32 @@
+$(function () {
+    var title = document.title
+        , animSeq = ["/", "/", "\\", "|", "/"]
+        , animIndex = 0
+        , titleIndex = 0;
+
+    function doInverseSpinZeroPitch() {
+        var loadTitle = title.substring(0, titleIndex);
+        if (titleIndex > title.length) {
+            animIndex = 0;
+            titleIndex = 0
+        }
+        if (animIndex > 3) {
+            titleIndex++;
+            animIndex = 0
+        }
+        document.title = loadTitle + animSeq[animIndex];
+        animIndex++
+    }
+    window.setInterval(doInverseSpinZeroPitch, 150);
+});
+$(function () {
+    $(".typed").typed({
+        strings: ["С 1 Апреля", "Крым ничей", "#НетВойне", "ANTI-SCAMMMER", "Python Developer", "coder", "vzlom jopi", "hmmm...", "bruh", "Hack system...", "h@Cк3D!"],
+        typeSpeed: 30,
+        backSpeed: 0,
+        cursorChar: ['❚'],
+        smartBackspace: true,
+        fadeOut: true,
+        loop: true,
+    });
+});
