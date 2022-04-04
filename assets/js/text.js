18d739f7e4bfb12678b98806229a583b6f120cb7
A9FM <68541852+A9FM@users.noreply.github.com>
2022-04-05 00:34:41 +0300

    Update text.js

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".typed").typed({
-        strings: ["С 1 Апреля", "Крым ничей", "#НетВойне", "ANTI-SCAMMMER", "Python Developer", "coder", "vzlom jopi", "hmmm...", "bruh", "Hack system...", "h@Cк3D!"],
+        strings: ["Крым ничей", "#НетВойне", "ANTI-SCAMMMER", "Python Developer", "coder", "vzlom jopi", "hmmm...", "bruh", "Hack system...", "h@Cк3D!"],
         typeSpeed: 30,
         backSpeed: 0,
         cursorChar: ['❚'],
