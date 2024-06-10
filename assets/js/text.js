0b6c0566a42b9e4f40fb86e644c54ab2a7351111
A9FM <126354695+A9fmOfficial@users.noreply.github.com>
2024-06-10 03:25:14 +0300

    BRUH BASE SITE RE-UPLOAD

---
+++ assets/js/text.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".typed").typed({
+        strings: ["Что-то между прогером и скрипт-кидди", "RT", "ANTI-SCAMMMER", "Python Developer", "coder", "vzlom jopi", "hmmm...", "bruh", "Hack system...", "h@Cк3D!"],
+        typeSpeed: 30,
+        backSpeed: 0,
+        cursorChar: ['❚'],
+        smartBackspace: true,
+        fadeOut: true,
+        loop: true,
+    });
+});
