056f26b03465e96adbdc61620b22a724fa41bb9f
ARCHI <ag387737@gmail.com>
2024-06-24 02:32:14 +0300

    #1151

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".typed").typed({
-        strings: ["Что-то между прогером и скрипт-кидди", "RT", "ANTI-SCAM", "Python Developer", "coder", "vzlom jopi", "hmmm...", "bruh", "KR4CK3", "Pashalko", "h@Cк3D!"],
+        strings: ["Что-то между прогером и скрипт-кидди", "RT", "ANTI-SCAM", "Python Developer", "coder", "vzlom jopi", "hmmm...", "bruh", "KR4CK3", "Pashalko", "h@Cк3D!", "#1151"],
         typeSpeed: 30,
         backSpeed: 0,
         cursorChar: ['❚'],
